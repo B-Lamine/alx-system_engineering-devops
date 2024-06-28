13dcee82c3881b9a2fafdb0ae67753df12d5a4ef
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-28 14:01:09 -0700

    1-install_a_package.pp: set default name

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # install version 2.1.0 of Flask
+# package resource name is used by default if argument absent
 
-package { 'install flask':
+package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3'
 }
