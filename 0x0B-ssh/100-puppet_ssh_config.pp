b26d54d3b05a005dc49ff0ea3592abbec329e9df
B.Lamine <medlamine.bellilet@outlook.com>
2024-06-30 16:28:50 -0700

    establish rsa key pair for server

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# maintain client-side configuration file
+
+ifile_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '~/.ssh/config',
+  line   => '\tIdentityFile ~/.ssh/school',
+  after  => '\tUser ubuntu'
+}
+
+file_line { 'PasswordAuthentication':
+  ensure => 'present',
+  path   => '~/.ssh/config',
+  line   => '\tPasswordAuthentication no',
+  after  => '\tUser ubuntu'
+}
---
+++ 0x0B-ssh/README.md
@@ -0,0 +1,1 @@
+Here are scripts for to establish and practice ssh connections to remote servers.
