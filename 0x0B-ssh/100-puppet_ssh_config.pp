ac89fa37da4f2c741cee810b119c90e74a8e8745
B.Lamine <medlamine.bellilet@outlook.com>
2024-07-01 05:27:57 -0700

    100-puppet_ssh_config.pp: use file  /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 file_line { 'IdentityFile':
   ensure => 'present',
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '	IdentityFile ~/.ssh/school',
   after  => '	User ubuntu',
 }
 
 file_line { 'PasswordAuthentication':
   ensure => 'present',
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '	PasswordAuthentication no',
   after  => '	User ubuntu',
 }
